e808c734cc5bca186a519204e4e98de884f14677
heatherzen <heather.zen@gmail.com>
2020-09-01 11:18:43 -0700

    finished 13.1

--- models/User.js
+++ models/User.js
@@ -6,13 +6,47 @@ class User extends Model {}
 
 // define table columns and configuration
 User.init(
-    {
-        // TABLE COMUMN DEFINITIONS GO HERE
+  {
+    // define an id column
+    id: {
+      // use the special Sequelize datatypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false, 
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increament
+      autoIncrement: true
+    }, 
+    // defien a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    {
-        // TABLE CONFIGURATION OPTION GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    email: {
+      type: DataTypes.STRING,
+       allowNull: false,
+       // there cannot be any duplicate email vzlues in this table
+       unique: true, 
+       // if allowNull is set to false, we can run our data through validators before creating the table data
+       validate: {
+         isEmail: true
+       }
+    },
+    // define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means the password mush ba at least four characters long
+        len: [4]
+      }
+    }
+  },
+  {
+    // TABLE CONFIGURATION OPTION GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
-          // pass in our imported sequelize connection (the direct connection to our database)
+    // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
@@ -26,4 +60,3 @@ User.init(
 );
 
 module.exports = User;
-    
