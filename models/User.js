98f10ae2b1815f0b87621b0958389639917729b1
heatherzen <heather.zen@gmail.com>
2020-09-01 12:21:44 -0700

    add hash encryption

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User Model
-class User extends Model {}
+class User extends Model { }
 
 // define table columns and configuration
 User.init(
@@ -12,12 +13,12 @@ User.init(
       // use the special Sequelize datatypes object provide what type of data it is
       type: DataTypes.INTEGER,
       // this is the equivalent of SQL's `NOT NULL` option
-      allowNull: false, 
+      allowNull: false,
       // instruct that this is the Primary Key
       primaryKey: true,
       // turn on auto increament
       autoIncrement: true
-    }, 
+    },
     // defien a username column
     username: {
       type: DataTypes.STRING,
@@ -25,13 +26,13 @@ User.init(
     },
     email: {
       type: DataTypes.STRING,
-       allowNull: false,
-       // there cannot be any duplicate email vzlues in this table
-       unique: true, 
-       // if allowNull is set to false, we can run our data through validators before creating the table data
-       validate: {
-         isEmail: true
-       }
+      allowNull: false,
+      // there cannot be any duplicate email vzlues in this table
+      unique: true,
+      // if allowNull is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true
+      }
     },
     // define a password column
     password: {
@@ -44,18 +45,31 @@ User.init(
     }
   },
   {
-    // TABLE CONFIGURATION OPTION GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
+    
+      
+  // TABLE CONFIGURATION OPTION GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
+  // pass in our imported sequelize connection (the direct connection to our database)
+  sequelize,
+  // don't automatically create createdAt/updatedAt timestamp fields
+  timestamps: false,
+  // don't pluralize name of database table
+  freezeTableName: true,
+  // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+  underscored: true,
+  // make it so our model name stays lowercase in the database
+  modelName: 'user'
   }
 );
 
